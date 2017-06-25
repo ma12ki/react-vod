437ee6fd7ea31ff0f899420b86bdaddcd1bfb23b
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-06-25 21:12:58 +0200

    added player

---
+++ client/src/Player/Player.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const Player = (props) => {
+    const { id, ext } = props; 
+    const type = getType(ext);
+    const url = `http://localhost:3001/play/${id}`;
+
+    return (
+        <video controls>
+            <source src={url} type={type} />
+            Your browser does not support the <code>video</code> element.
+        </video>
+    );
+};
+
+const getType = (ext) => {
+    switch (ext) {
+        case '.mp4':
+            return 'video/mp4';
+        case '.webm':
+            return 'video/webm';
+        default:
+            throw new Error(`Unknown format: ${ext}`);
+    }
+};
+
+export default Player;
--- client/src/Videos/Video/Video.js
+++ client/src/Videos/Video/Video.js
@@ -5,12 +5,14 @@ import Link from 'redux-first-router-link';
 import { routesKeys } from '../routes';
 import { payload } from '../../router.selectors';
 import { videosEntities } from '../videos.selectors';
+import Player from '../../Player/Player';
 
 export const Video = (props) => {
     const { video } = props;
     const { id, name, ext, title, size, duration, dateCreated, dateModified } = video; 
     return (
         <div>
+            <Player id={id} ext={ext} />
             {name}{ext} - {title}
             <Link href={{ type: routesKeys.home }}>Home</Link>
         </div>
