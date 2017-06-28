58fd3a430e08d7750c5186f618dff96d0df4a877
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-06-28 20:50:06 +0200

    used express's sendFile instead of service

--- server/src/streamer/streamer.controller.ts
+++ server/src/streamer/streamer.controller.ts
@@ -6,17 +6,22 @@ import { IVideoFile } from '../store';
 import { IStreamer } from './streamer.service';
 import { streamerTokens } from './streamer.tokens';
 
+import { storeTokens, IVideoStore } from '../store';
+
 @Controller('/play')
 @injectable()
 export class StreamerController implements interfaces.Controller {
 
     constructor(
         @inject(streamerTokens.streamerService) private streamerService: IStreamer,
+        @inject(storeTokens.videoStore) private store: IVideoStore,
     ) {}
 
     @Get('/:id')
     public async stream(req: express.Request, res: express.Response, next: express.NextFunction): Promise<any> {
         const { id } = req.params;
-        return this.streamerService.stream(id);
+        // return this.streamerService.stream(id);
+        const file = await this.store.getOne(id);
+        return res.sendFile(file.path);
     }
 }
