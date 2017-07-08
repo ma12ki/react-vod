9bd682759b2a259ad090b3f774beca4d52f26742
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-07-08 20:31:50 +0200

    fixed config file

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -2,18 +2,12 @@ import { config as dotenvConfig } from 'dotenv';
 
 const env = dotenvConfig();
 
-console.log(env);
-
 const production = process.env.NODE_ENV === 'production';
-const dirs = process.env.APP_VIDEO_DIRS.split(',').map((dir: string) => dir.trim());
+const dirs = (process.env.APP_VIDEO_DIRS || '').split(',').map((dir: string) => dir.trim());
 
 const config = {
-  get production() {
-      return production;
-  },
-  get dirs() {
-    return dirs;
-  },
+    production,
+    dirs,
 };
 
 export { config };
