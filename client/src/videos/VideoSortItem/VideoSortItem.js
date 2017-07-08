35c6ff742ee65106f49a138b75446e2e221577f1
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-07-08 23:44:19 +0200

    added sort icons

--- client/src/videos/VideoSortItem/VideoSortItem.js
+++ client/src/videos/VideoSortItem/VideoSortItem.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import FA from 'react-fontawesome';
+
+import styles from './videoSortItem.module.css';
 
 export const VideoSortItem = ({ className, currentSortCol, currentSortDir, colName, onSort, children }) => {
+    const active = colName === currentSortCol && currentSortDir;
+    const icon = active ?
+        <FA name={`sort-${currentSortDir}`} className={styles.sortIcon} /> :
+        <FA name='sort' className={styles.sortIcon} />;
     return (
-        <th className={className} onClick={() => onSort({ col: colName, dir: getNextDir(colName, currentSortCol, currentSortDir)})}>{children}</th>
+        <th className={`${className} ${styles.interactive} ${active ? '' : styles.inactive}`} onClick={() => onSort({ col: colName, dir: getNextDir(colName, currentSortCol, currentSortDir)})}>
+            {children}{icon}
+        </th>
     );
 };
 
