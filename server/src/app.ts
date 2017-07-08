5e63662302710c794ccd464ac816a9f51a8e5486
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-07-08 18:00:00 +0200

    split server.ts into 2 files

---
+++ server/src/app.ts
@@ -0,0 +1,45 @@
+/**
+ * Module dependencies.
+ */
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as errorHandler from 'errorhandler';
+import * as logger from 'morgan';
+import * as cors from 'cors';
+import expressValidator = require('express-validator');
+import { Request, Response } from 'express';
+import { InversifyExpressServer } from 'inversify-express-utils';
+
+import { container } from './config';
+
+/**
+ * Create Express server.
+ */
+const server = new InversifyExpressServer(container);
+
+/**
+ * Express configuration.
+ */
+server.setConfig((app) => {
+    app.set('port', process.env.PORT || 3001);
+    app.use(logger('dev'));
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(expressValidator());
+});
+
+/**
+ * Error Handler. Provides full stack - remove for production
+ */
+server.setErrorConfig((app) => {
+    app.use(errorHandler());
+});
+
+/**
+ * Build the server.
+ */
+
+const app = server.build();
+
+export { app };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,55 +1,8 @@
-/**
- * Module dependencies.
- */
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import * as errorHandler from 'errorhandler';
-import * as logger from 'morgan';
-import * as cors from 'cors';
-import * as dotenv from 'dotenv';
-import * as path from 'path';
-import expressValidator = require('express-validator');
-import { Request, Response } from 'express';
-import { InversifyExpressServer } from 'inversify-express-utils';
 import 'reflect-metadata';
 
+import { app } from './app';
 import { container } from './config';
 import { readerTokens, IReader } from './reader';
-
-/**
- * Controllers (route handlers).
- */
-
-/**
- * Create Express server.
- */
-const server = new InversifyExpressServer(container);
-
-/**
- * Express configuration.
- */
-server.setConfig((app) => {
-    app.set('port', process.env.PORT || 3001);
-    app.use(logger('dev'));
-    app.use(cors());
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(expressValidator());
-});
-
-/**
- * Error Handler. Provides full stack - remove for production
- */
-server.setErrorConfig((app) => {
-    app.use(errorHandler());
-});
-
-
-/**
- * Start Express server.
- */
-
-const app = server.build();
 
 app.listen(app.get('port'), () => {
   console.log(('  App is running at http://localhost:%d in %s mode'), app.get('port'), app.get('env'));
