491091696cbd189a6a9fbe844d7f7307816f6eaf
Marcin Zolkiewski <Marcin_Zolkiewski@epam.com>
2017-07-10 21:52:09 +0200

    configured docker for production

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ import { app } from './app';
 import { container } from './config';
 import { readerTokens, IReader } from './reader';
 
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), '0.0.0.0', () => {
   console.log(('  App is running at http://localhost:%d in %s mode'), app.get('port'), app.get('env'));
   console.log('  Press CTRL-C to stop\n');
 });
