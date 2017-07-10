80c645c8ff40525fd7143a4c94702d1d910456d0
Marcin Zolkiewski <Marcin_Zolkiewski@epam.com>
2017-07-10 22:44:32 +0200

    added check before reading .env

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,6 +1,13 @@
+import { accessSync } from 'fs';
+import { resolve } from 'path';
 import { config as dotenvConfig } from 'dotenv';
 
-const env = dotenvConfig();
+try {
+    accessSync(resolve(__dirname, '..', '.env'));
+    const env = dotenvConfig();
+} catch (e) {
+    console.warn(`Warning: loading .env failed: ${e}`);
+}
 
 const production = process.env.NODE_ENV === 'production';
 const dirs = (process.env.APP_VIDEO_DIRS || '').split(',').map((dir: string) => dir.trim());
